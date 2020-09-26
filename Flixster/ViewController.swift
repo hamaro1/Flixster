6ff34ac59a940acc13d6d4ab7fc00a04631706dd
hamaro01 <Katherine.helena01@gmail.com>
2020-09-25 22:07:38 -0400

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Helen Amaro on 9/25/20.
+//  Copyright © 2020 helenamaro@ymail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
